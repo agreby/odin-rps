0b318f1d540b45ea75c55b01f4b122d5c4c5d6a0
Khalil Agrebi <foulmoul@gmail.com>
2023-10-01 00:03:11 +0100

    Change console log methods into DOM methods

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
 function getComputerChoise(){
     let items = ["rock", "paper", "scissors"];
     let comp_choice = items[Math.floor(Math.random()*items.length)];
-    console.log(comp_choice);
     return comp_choice;
 }
 
@@ -43,37 +42,65 @@ function playRound(playerSelection,getComputerChoise){
                 break;
         }
     }
-    console.log(victorp1);
-    console.log(victorai);
-}
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    div.textContent = `ai = "${victorai}" vs p1 = "${victorp1}"`;
+    body.appendChild(div);
+    const divs = document.querySelectorAll('div');
+    if(victorp1 == 5 || victorai == 5){
+            divs.forEach(div => {
+                div.remove();
+            });
+        }
+    }
 
-function playerSelection(){
+function playerSelection(x){
     while(true){
-        var player_choice = prompt("what will you choose");
+        var player_choice = x;
         player_choice = player_choice.toLowerCase();
         if(player_choice == "rock" || player_choice == "paper" || player_choice == "scissors"){
             break;
         }
     }
-    console.log(player_choice);
     return player_choice;
 }
-
+/*
 function game(){
-    while (victorp1 != 5 || victorp1 != 5){
+    while (victorp1 != 1 || victorp1 != 1){
         playRound(playerSelection(),getComputerChoise());
-        if(victorp1 == 5){
+        if(victorp1 == 1){
             alert("player won the game");
             break;
         }
-        else if(victorai == 5){
+        else if(victorai == 1){
             alert("the computer won the game");
             break;
         }
     }
     
 }
+*/
+function game(){
+    window.addEventListener("click",function(e){
+        const btn = document.querySelector(`button[class="${e.target.getAttribute("class")}"]`);
+        if(!btn){
+            return;
+        }
+        const choice = btn.getAttribute("class");
+        playRound(playerSelection(choice),getComputerChoise());
+        if(victorp1 == 5){
+            alert("player won the game");
+            victorp1 = 0;
+            victorai = 0;
+        }
+        else if(victorai == 5){
+            alert("the computer won the game");
+            victorp1 = 0;
+            victorai = 0;
+        }
 
+    });
+}
 
 let victorp1 = 0;
 let victorai = 0;
